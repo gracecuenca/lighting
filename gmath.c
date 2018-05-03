3eedf240997ad3859054333287dd63c92f7cfb37
gracecuenca <gcuenca@stuy.edu>
2018-05-03 17:14:53 -0400

    needs testing

--- gmath.c
+++ gmath.c
@@ -9,6 +9,16 @@
 //lighting functions
 color get_lighting( double *normal, double *view, color alight, double light[2][3], double *areflect, double *dreflect, double *sreflect) {
   color i;
+  normalize(light[LOCATION]);
+  normalize(normal);
+  normalize(view);
+  color a = calculate_ambient(alight, areflect);
+  color d = calculate_diffuse(light, dreflect, normal);
+  color s = calculate_specular(light, sreflect, view, normal);
+  i.red = a.red + d.red + s.red;
+  i.green = a.green + d.green + s.green;
+  i.blue = a.blue + d.blue + s.blue;
+  limit_color(&i);
   return i;
 }
 
@@ -16,24 +26,40 @@ color calculate_ambient(color alight, double *areflect ) {
   //A * Ka
   color a;
   //red, green, blue
-  a.red = alight.red * areflect[0];
-  a.blue = alight.blue * areflect[1];
-  a.green = alight.green * areflect[2];
+  a.red = alight.red * areflect[RED];
+  a.green = alight.green * areflect[GREEN];
+  a.blue = alight.blue * areflect[BLUE];
   return a;
 }
 
 color calculate_diffuse(double light[2][3], double *dreflect, double *normal ) {
   //costheta L(normalized) * n(normalized)
+  //PKd(N(normalized * I(normalized)))
+  //might be over way around
+  double dot = dot_product(light[LOCATION], normal);
   color d;
-  d.red =
-  d.green =
-  d.blue = 
+  d.red = light[COLOR][RED] * dot * dreflect[RED];
+  d.green = light[COLOR][GREEN] * dot * dreflect[GREEN];
+  d.blue = light[COLOR][GREEN] * dot * dreflect[BLUE];
   return d;
 }
 
+//dot product orders might be messed up
 color calculate_specular(double light[2][3], double *sreflect, double *view, double *normal ) {
-
   color s;
+  //PKs[2(N * L) x N - L * v]
+  double R = 2*dot_product(normal, light[LOCATION]);
+  normal[RED] = normal[RED] * R;
+  normal[GREEN] = normal[GREEN] * R;
+  normal[BLUE] = normal[BLUE] * R;
+  //cos between r and v = r * v
+  double * reflected = normal;
+  double rv = dot_product(reflected, view);
+  rv = rv > 0 ? rv : 0;
+  rv = pow(rv, SPECULAR_EXP);
+  s.red = light[COLOR][RED] * rv * sreflect[RED];
+  s.green = light[COLOR][GREEN] * rv * sreflect[GREEN];
+  s.red = light[COLOR][BLUE] * rv * sreflect[BLUE];
   return s;
 }
 
@@ -51,10 +77,10 @@ void limit_color( color * c ) {
 //vector functions
 //normalize vetor, should modify the parameter
 void normalize( double *vector ) {
-  double L = sqrt(vector[0] * vector[0] + vector[1] * vector[1] + vector[2] * vector[2]);
-  vector[0] = vector[0]/L;
-  vector[1] = vector[1]/L;
-  vector[2] = vector[2]/L;
+  double L = sqrt(vector[RED] * vector[RED] + vector[GREEN] * vector[GREEN] + vector[BLUE] * vector[BLUE]);
+  vector[RED] = vector[RED]/L;
+  vector[GREEN] = vector[GREEN]/L;
+  vector[BLUE] = vector[BLUE]/L;
 }
 
 //Return the dot porduct of a . b
